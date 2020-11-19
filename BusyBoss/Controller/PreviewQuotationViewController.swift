f2c76133b926cbbb9ca6ee5f632e82f3bc36ecd8
andreado990 <47097723+andreado990@users.noreply.github.com>
2020-11-19 18:22:02 +0700

    update data PDFPreview 19/11/2020 18.21

---
+++ BusyBoss/Controller/PreviewQuotationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PreviewQuotationViewController.swift
+//  BusyBoss
+//
+//  Created by Andre Marines ado Tena Uak on 16/11/20.
+//
+
+import UIKit
+import PDFKit
+
+class PreviewQuotationViewController: UIViewController {
+    
+    @IBOutlet weak var pdfView : PDFView!
+    
+    public var documentData: Data?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        if let data = documentData {
+          pdfView.document = PDFDocument(data: data)
+          pdfView.autoScales = true
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
