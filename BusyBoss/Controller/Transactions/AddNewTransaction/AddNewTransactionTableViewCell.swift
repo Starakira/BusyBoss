3e07329db0ec54820695f62337213b171bfe2415
Muhammad Bangun Agung <bangunagung1@gmail.com>
2020-12-03 14:33:51 +0700

    Fixing quantity

--- BusyBoss/Controller/Transactions/AddNewTransaction/AddNewTransactionTableViewCell.swift
+++ BusyBoss/Controller/Transactions/AddNewTransaction/AddNewTransactionTableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AddNewTransactionTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var productNameLabel: UILabel!
     @IBOutlet weak var transactionProductQuantityLabel: UILabel!
@@ -18,27 +18,27 @@ class AddNewTransactionTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     func setProductQuantity(transaction: Transaction, product: Product?) {
-            if let transactionQuantity = product?.transactionQuantity {
-                self.transactionProductQuantityLabel.text = String(transactionQuantity)
-            }else {
-                CloudKitManager.shared().transactionFetchProductQuantity(transactionID: transaction.recordID!, productID: (product?.recordID)!) {
-                    (quantity, error) in
-                    if let error = error {
-                        print(error.localizedDescription)
-                    } else {
-                        DispatchQueue.main.async {
-                            self.transactionProductQuantityLabel.text = String(quantity)
-                        }
+        if let transactionQuantity = product?.transactionQuantity {
+            self.transactionProductQuantityLabel.text = String(transactionQuantity)
+        }else {
+            CloudKitManager.shared().transactionFetchProductQuantity(transactionID: transaction.recordID!, productID: (product?.recordID)!) {
+                (quantity, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    DispatchQueue.main.async {
+                        self.transactionProductQuantityLabel.text = String(quantity)
                     }
                 }
             }
         }
+    }
 }
