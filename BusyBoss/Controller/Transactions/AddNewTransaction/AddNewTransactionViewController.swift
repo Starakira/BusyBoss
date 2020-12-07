ccc0ca33208618f75e5483edf1f825ae8bf4963a
Muhammad Bangun Agung <bangunagung1@gmail.com>
2020-12-07 11:37:46 +0700

    Added IQKeyboardManager using Cocoapods

--- BusyBoss/Controller/Transactions/AddNewTransaction/AddNewTransactionViewController.swift
+++ BusyBoss/Controller/Transactions/AddNewTransaction/AddNewTransactionViewController.swift
@@ -76,9 +76,6 @@ class AddNewTransactionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
         totalProductsPriceLabel.text = "Rp " + decimalFormatter.string(for: totalProductPrice)!
         
         discountTextField.isHidden = true
@@ -174,20 +171,6 @@ class AddNewTransactionViewController: UIViewController {
             vc.clientsListDelegate = self
         } else if let vc = segue.destination as? AddTransactionProductsViewController {
             vc.myDelegate = self
-        }
-    }
-    
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height
-            }
-        }
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
         }
     }
     
